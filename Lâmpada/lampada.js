5bcc12cbeaba2b25162330a5f37676eea358ddb0
Iuri-de-Deus <113307346+Iuri-de-Deus@users.noreply.github.com>
2023-02-04 16:21:20 -0300

    Add files via upload

---
+++ Lâmpada/lampada.js
@@ -0,0 +1,29 @@
+const turnOn = document.getElementById ('turnOn');
+const turnOff = document.getElementById ('turnOff');
+const lamp = document.getElementById ('lamp');
+
+function islampBroken () {
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
+function lampOn () {
+    if ( !islampBroken () ) {
+    lamp.src = './img/ligada.jpg';
+    }
+}
+
+function lampOff () {
+    if ( !islampBroken () ) {
+    lamp.src = './img/desligada.jpg';
+    }
+}
+
+function lampBroken () {
+    lamp.src = './img/quebrada.jpg';
+}
+
+turnOn.addEventListener ( 'click', lampOn);
+turnOff.addEventListener ('click', lampOff);
+lamp.addEventListener ('mouseover', lampOn);
+lamp.addEventListener ('mouseleave', lampOff);
+lamp.addEventListener ('dblclick', lampBroken);
